b208058ba8d4c0227c5ce99d6a38c0cb1a5d804b
peaceman <nixalio@gmail.com>
2018-09-09 14:27:56 +0200

    Update to laravel 5.7

--- config/logging.php
+++ config/logging.php
@@ -60,6 +60,16 @@ return [
             'level' => 'critical',
         ],
 
+        'papertrail' => [
+            'driver'  => 'monolog',
+            'level' => 'debug',
+            'handler' => SyslogUdpHandler::class,
+            'handler_with' => [
+                'host' => env('PAPERTRAIL_URL'),
+                'port' => env('PAPERTRAIL_PORT'),
+            ],
+        ],
+
         'stderr' => [
             'driver' => 'monolog',
             'handler' => StreamHandler::class,
