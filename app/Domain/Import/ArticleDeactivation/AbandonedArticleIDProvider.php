4393c2e8affce45e0ea84660e74b04ba6da8109f
peaceman <nixalio@gmail.com>
2018-10-30 10:37:29 +0100

    BugFix: Adjust abandoned article query for mysql compatibility

--- app/Domain/Import/ArticleDeactivation/AbandonedArticleIDProvider.php
+++ app/Domain/Import/ArticleDeactivation/AbandonedArticleIDProvider.php
@@ -10,7 +10,8 @@ class AbandonedArticleIDProvider implements ArticleIDProvider
 {
     public function getArticleIDs(): iterable
     {
-        $importFileQuery = <<<EOT
+        $importFileQuery = /** @lang MySQL */
+            <<<EOT
 select id
 from import_files
 where import_files.type = 'base'
@@ -21,16 +22,17 @@ EOT;
         $importFileIds = collect(DB::select($importFileQuery))->pluck('id');
         $importFileIdPlaceholders = $importFileIds->map(function () { return '?'; })->implode(', ');
 
-        $query = <<<EOT
+        $query = /** @lang MySQL */
+            <<<EOT
 select a.id,
        (select count(1)
         from article_imports
         where article_imports.article_id = a.id
-          and article_imports.import_file_id in ($importFileIdPlaceholders)) occurrences_in_last_7_imports
+          and article_imports.import_file_id in ($importFileIdPlaceholders)) as occurrences_in_last_7_imports
 from articles as a
-where occurrences_in_last_7_imports = 0
-and a.sw_article_id is not null
+where a.sw_article_id is not null
 and a.is_active = 1
+having occurrences_in_last_7_imports = 0
 EOT;
 
         $result = DB::select($query, $importFileIds->toArray());
