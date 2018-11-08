cf21f49e61b2d74471fb71d2a5575709d9432bca
peaceman <nixalio@gmail.com>
2018-11-08 22:28:52 +0100

    Add quantity to the daily overview

--- resources/views/emails/orders/daily-overview.blade.php
+++ resources/views/emails/orders/daily-overview.blade.php
@@ -2,11 +2,11 @@
 # Daily Order Overview
 
 @component('mail::table')
-    | OrderNumber | OrderTime | ArticleNumber | ArticleName |
-    | ------------|:---------:| -------------:|------------:|
+    | OrderNumber | OrderTime | ArticleNumber | Quantity | ArticleName |
+    | ------------|:---------:| -------------:|---------:|------------:|
     @foreach($orders as $order)
     @foreach($order->orderArticles as $orderArticle)
-    | {{ $order->sw_order_number }} | {{ $order->sw_order_time->format(DateTime::ISO8601) }} | {{ $orderArticle->sw_article_number }} | {{ $orderArticle->sw_article_name }} |
+    | {{ $order->sw_order_number }} | {{ $order->sw_order_time->format(DateTime::ISO8601) }} | {{ $orderArticle->sw_article_number }} | {{ $orderArticle->sw_quantity }} | {{ $orderArticle->sw_article_name }} |
     @endforeach
     @endforeach
 @endcomponent
