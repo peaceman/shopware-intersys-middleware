5d8cadcab89dd514d6351ef9487d07578f43468c
peaceman <nixalio@gmail.com>
2020-04-04 07:10:40 +0200

    Unindent the markdown mail template

--- resources/views/emails/orders/daily-overview.blade.php
+++ resources/views/emails/orders/daily-overview.blade.php
@@ -2,13 +2,13 @@
 # Daily Order Overview
 
 @component('mail::table')
-    | OrderNumber | OrderTime | ArticleNumber | Quantity | ArticleName |
-    | ------------|:---------:| -------------:|---------:|------------:|
-    @foreach($orders as $order)
-    @foreach($order->orderArticles as $orderArticle)
-    | {{ $order->sw_order_number }} | {{ $order->sw_order_time->format(DateTime::ISO8601) }} | {{ $orderArticle->sw_article_number }} | {{ $orderArticle->sw_quantity }} | {{ $orderArticle->sw_article_name }} |
-    @endforeach
-    @endforeach
+| OrderNumber | OrderTime | ArticleNumber | Quantity | ArticleName |
+| ------------|:---------:| -------------:|---------:|------------:|
+@foreach($orders as $order)
+@foreach($order->orderArticles as $orderArticle)
+| {{ $order->sw_order_number }} | {{ $order->sw_order_time->format(DateTime::ISO8601) }} | {{ $orderArticle->sw_article_number }} | {{ $orderArticle->sw_quantity }} | {{ $orderArticle->sw_article_name }} |
+@endforeach
+@endforeach
 @endcomponent
 
 Thanks,<br>
